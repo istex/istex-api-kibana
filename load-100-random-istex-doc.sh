92ac33467c533ec1027c9fbcd0ea7279dfc1b034
Stéphane Gully <stephane.gully@gmail.com>
2017-11-19 13:47:49 +0100

    upgrade to ELK v6 (use https://www.elastic.co/guide/en/kibana/6.0/migrating-6.0-index.html to migrate index for kibana)

--- load-100-random-istex-doc.sh
+++ load-100-random-istex-doc.sh
@@ -5,6 +5,6 @@ do
   ISTEX_JSON_DOC=$(curl -s "https://api.istex.fr/document/?q=*&output=*&rankBy=random&size=1&sid=istex-kibana" | jq '.hits[0]')
   ISTEX_ID=$(echo $ISTEX_JSON_DOC | jq -r .id)
   ISTEX_CORPUS=$(echo $ISTEX_JSON_DOC | jq -r .corpusName)
-  echo $ISTEX_JSON_DOC | curl -s -XPUT "http://localhost:9200/istex-corpus-$ISTEX_CORPUS/doc/$ISTEX_ID" -d @- | \
+  echo $ISTEX_JSON_DOC | curl -s -XPUT -H 'Content-Type: application/json' "http://localhost:9200/istex-corpus-$ISTEX_CORPUS/doc/$ISTEX_ID" -d @- | \
     jq -r "\"Document #$DOC_IDX $ISTEX_ID ($ISTEX_CORPUS): \(.result)\""
 done
