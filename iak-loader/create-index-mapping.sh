6f45409be84fd4f5a6c2492643960fe4b779f012
Stéphane Gully <stephane.gully@gmail.com>
2017-11-26 20:08:25 +0100

    better output for index mapping creation script

--- iak-loader/create-index-mapping.sh
+++ iak-loader/create-index-mapping.sh
@@ -6,30 +6,33 @@ CORPUS_LIST=$(curl -s $IAK_ISTEX_API_URL/corpus/ | jq -r '.[].key')
 
 for CORPUS in $CORPUS_LIST
 do
-  curl -XPUT "$ELASTICSEARCH_URL/istex-corpus-${CORPUS}?pretty" -H 'Content-Type: application/json' -d'
-  {
-    "mappings": {
-      "doc": {
-        "properties": {
-          "host": {
-            "properties": {
-              "publicationDate": {
-                "type": "date"
-              },
-              "copyrightDate": {
-                "type": "date"
+  echo '
+    {
+      "mappings": {
+        "doc": {
+          "properties": {
+            "host": {
+              "properties": {
+                "publicationDate": {
+                  "type": "date"
+                },
+                "copyrightDate": {
+                  "type": "date"
+                }
               }
-            }
-          },
-          "publicationDate": {
-            "type": "date"
-          },
-          "copyrightDate": {
-            "type": "date"
-          }          
+            },
+            "publicationDate": {
+              "type": "date"
+            },
+            "copyrightDate": {
+              "type": "date"
+            }          
+          }
         }
       }
     }
-  }
-  '
+  ' | curl -s -H 'Content-Type: application/json' \
+           -XPUT "$ELASTICSEARCH_URL/istex-corpus-${CORPUS}?pretty" \
+           -d @- \
+    | jq -r '"Mapping créé pour cet index: \(.index)"'
 done
--- iak-loader/wait-for-elasticsearch-to-be-ready.sh
+++ iak-loader/wait-for-elasticsearch-to-be-ready.sh
@@ -6,7 +6,7 @@ while [ $ret -gt 0 ]
 do
   ES_STATUS=$(curl -s "$ELASTICSEARCH_URL/_cluster/health?wait_for_status=yellow&timeout=5s" | jq -r .status)
   if [ "${ES_STATUS}" == "red" ] || [ "${ES_STATUS}" == "" ] ; then
-    echo -n "."
+    # echo -n "."
     sleep 1s
     ret=1
   else
