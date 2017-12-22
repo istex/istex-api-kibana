ab7ed836fd58aeee042d35bc0900050342713b81
Stéphane Gully <stephane.gully@gmail.com>
2017-12-22 17:46:46 +0100

    Only one node so do not replicate anything

--- iak-loader/create-index-mapping.sh
+++ iak-loader/create-index-mapping.sh
@@ -2,6 +2,14 @@
  
 wait-for-elasticsearch-to-be-ready.sh
 
+echo -n "Only one node so do not replicate anything:"
+echo '{
+    "index.number_of_replicas" : "0"
+}' | curl -s -H 'Content-Type: application/json' \
+          -XPUT "$ELASTICSEARCH_URL/_all/_settings?preserve_existing=true" \
+          -d @-
+echo ""
+
 CORPUS_LIST=$(curl -s $IAK_ISTEX_API_URL/corpus/ | jq -r '.[].key')
 
 for CORPUS in $CORPUS_LIST
@@ -35,4 +43,13 @@ do
            -XPUT "$ELASTICSEARCH_URL/istex-corpus-${CORPUS}?pretty" \
            -d @- \
     | jq -r '"Mapping created for this index: \(.index)"'
+
+
+  # only one node so do not replicate anything
+  echo '{
+      "index.number_of_replicas" : "0"
+  }' | curl -s -H 'Content-Type: application/json' \
+            -XPUT "$ELASTICSEARCH_URL/istex-corpus-${CORPUS}/_settings" \
+            -d @- \
+    | jq -r '"Replicas set to 0 because only one node: \(.acknowledged)"'
 done
